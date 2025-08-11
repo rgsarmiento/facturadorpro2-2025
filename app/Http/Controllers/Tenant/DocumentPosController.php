cae3923b7949636f67d58af958c7e655f49da7db
Alexander Obando <gerencia@torresoftware.com>
2025-08-11 15:46:33 -0500

    Correcciones en inventario notas credito

--- app/Http/Controllers/Tenant/DocumentPosController.php
+++ app/Http/Controllers/Tenant/DocumentPosController.php
@@ -1776,6 +1776,7 @@ class DocumentPosController extends Controller
                     }
                     $sale_note_item->fill($row);
                     $sale_note_item->document_pos_id = $this->sale_note->id;
+                    $sale_note_item->refund = true;
                     $sale_note_item->save();
                     if(isset($row['lots'])){
                         foreach($row['lots'] as $lot) {
