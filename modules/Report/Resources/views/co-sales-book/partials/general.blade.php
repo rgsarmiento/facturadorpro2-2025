b85164342b457160aa621510cd26bae72670bcce
Alexander Obando <gerencia@torresoftware.com>
2025-10-20 12:00:48 -0500

    Fix FINAL: Agregar operador ?? a tax->name en TODAS las vistas Blade - Protege contra acceso a propiedad inexistente incluso dentro de isset() - summary.blade.php: 2 lugares corregidos - general.blade.php: 1 lugar corregido

--- modules/Report/Resources/views/co-sales-book/partials/general.blade.php
+++ modules/Report/Resources/views/co-sales-book/partials/general.blade.php
@@ -9,7 +9,7 @@
                     <th colspan="2">
                         IMPUESTO #{{ $loop->iteration }}
                         <br>
-                        {{ $tax->name }} - ({{ $tax->rate ?? '0' }}%)
+                        {{ $tax->name ?? 'Impuesto sin nombre' }} - ({{ $tax->rate ?? '0' }}%)
                     </th>
                 @endif
             @endforeach
