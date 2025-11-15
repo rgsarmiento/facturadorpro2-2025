d5fdcd612e73d64bc10aa31714440ae71a2ebc79
Alexander Obando <gerencia@torresoftware.com>
2025-11-15 08:01:21 -0500

    Se mejora aspecto de algunos formularios - compilado a produccion

--- modules/Purchase/Resources/assets/js/views/purchase-quotations/form.vue
+++ modules/Purchase/Resources/assets/js/views/purchase-quotations/form.vue
@@ -1,124 +1,139 @@
 <template>
-    <div class="card mb-0 pt-2 pt-md-0">
-        <!-- <div class="card-header bg-info">
-            <h3 class="my-0">Nuevo Comprobante</h3>
-        </div> -->
+    <div class="card mb-0 pt-2 pt-md-0 invoice-form">
+        <div class="card-header bg-info">
+            <h4 class="mb-0 text-white"><i class="fas fa-file-invoice"></i> Cotización de Compra</h4>
+        </div>
         <div class="card-body" v-if="loading_form">
             <div class="invoice">
-                <header class="clearfix">
-                    <div class="row">
-                        <div class="col-sm-2 text-center mt-3 mb-0">
-                            <logo url="/" :path_logo="(company.logo != null) ? `/storage/uploads/logos/${company.logo}` : ''" ></logo>
-                        </div>
-                        <div class="col-sm-10 text-left mt-3 mb-0">
-                            <address class="ib mr-2" >
-                                <span class="font-weight-bold d-block">COTIZACIÓN</span>
-                                <span class="font-weight-bold d-block">COTC-XXX</span>
-                                <span class="font-weight-bold">{{company.name}}</span>
-                                <br>
-                                <div v-if="establishment.address != '-'">{{ establishment.address }}, </div> {{ establishment.city.name }}, {{ establishment.department.name }} - {{ establishment.country.name }}
-                                <br>
-                                {{establishment.email}} - <span v-if="establishment.telephone != '-'">{{establishment.telephone}}</span>
-                            </address>
-                        </div>
-                    </div>
-                </header>
                 <form autocomplete="off" @submit.prevent="submit">
                     <div class="form-body">
-                        <div class="row mt-1">
 
-                            <div class="col-lg-10 col-md-10">
-
-                                <table width="100%">
-                                    <thead>
-                                        <tr width="100%">
-                                            <th width="55%" v-if="form.suppliers.length>0" class="pb-2">Proveedor
-                                                <a href="#" class="text-center font-weight-bold text-info" @click.prevent="showDialogNewPerson = true">[+ Nuevo]</a>
-                                            </th>
-                                            <th width="30%" v-if="form.suppliers.length>0" class="pb-2">Correo electrónico</th>
-                                            <th width="15%"><a href="#" @click.prevent="clickAddSupplier" class="text-center font-weight-bold text-info">[+ Agregar]</a></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr v-for="(row, index) in form.suppliers" :key="index" width="100%">
-                                            <td width="55%" >
-                                                <div class="form-group mb-1 mr-2">
-                                                    <el-select v-model="row.supplier_id" filterable @change="changeSupplier(index)">
-                                                        <el-option v-for="option in suppliers" :key="option.id" :value="option.id" :label="option.description"></el-option>
-                                                    </el-select>
-                                                </div>
-                                            </td>
-                                            <td width="30%" >
-                                                <div class="form-group mb-1 mr-2"  >
-                                                    <el-input v-model="row.email"></el-input>
-                                                </div>
-                                            </td>
-                                            <td width="15%"  class="series-table-actions text-center" v-if="index > 0">
-                                                <button  type="button" class="btn waves-effect waves-light btn-xs btn-danger" @click.prevent="clickCancel(index)">
-                                                    <i class="fa fa-trash"></i>
-                                                </button>
-                                            </td>
-                                            <br>
-                                        </tr>
-                                    </tbody>
-                                </table>
-
-
-                            </div>
-
-                            <div class="col-lg-2">
-                                <div class="form-group" :class="{'has-danger': errors.date_of_issue}">
-                                    <!--<label class="control-label">Fecha de emisión</label>-->
-                                    <label class="control-label">Fec. Emisión</label>
-                                    <el-date-picker v-model="form.date_of_issue" type="date" value-format="yyyy-MM-dd" :clearable="false" @change="changeDateOfIssue"></el-date-picker>
-                                    <small class="form-control-feedback" v-if="errors.date_of_issue" v-text="errors.date_of_issue[0]"></small>
+                        <!-- Encabezado de la Empresa -->
+                        <div class="form-section">
+                            <h5 class="section-header"><i class="fas fa-building"></i> Información de la Empresa</h5>
+                            <div class="row">
+                                <div class="col-sm-2 text-center">
+                                    <logo url="/" :path_logo="(company.logo != null) ? `/storage/uploads/logos/${company.logo}` : ''" ></logo>
+                                </div>
+                                <div class="col-sm-10">
+                                    <address class="ib mr-2">
+                                        <span class="font-weight-bold d-block">COTIZACIÓN</span>
+                                        <span class="font-weight-bold d-block">COTC-XXX</span>
+                                        <span class="font-weight-bold">{{company.name}}</span>
+                                        <br>
+                                        <div v-if="establishment.address != '-'">{{ establishment.address }}, </div> {{ establishment.city.name }}, {{ establishment.department.name }} - {{ establishment.country.name }}
+                                        <br>
+                                        {{establishment.email}} - <span v-if="establishment.telephone != '-'">{{establishment.telephone}}</span>
+                                    </address>
                                 </div>
                             </div>
-
                         </div>
 
-                        <div class="row mt-3">
-                            <div class="col-md-12">
-                                <div class="table-responsive">
-                                    <table class="table">
+                        <!-- Proveedores y Fecha -->
+                        <div class="form-section">
+                            <h5 class="section-header"><i class="fas fa-user-tie"></i> Proveedores y Fecha de Emisión</h5>
+                            <div class="row">
+                                <div class="col-lg-10 col-md-10">
+                                    <table width="100%">
                                         <thead>
                                             <tr width="100%">
-                                                <th width="5%">#</th>
-                                                <th width="50%" class="font-weight-bold">Descripción</th>
-                                                <th width="15%" class="text-center font-weight-bold">Unidad</th>
-                                                <th width="15%" class="text-right font-weight-bold">Cantidad</th>
-                                                <th width="15%"></th>
+                                                <th width="55%" v-if="form.suppliers.length>0" class="pb-2">Proveedor
+                                                    <el-tooltip class="item" effect="dark" content="Crear nuevo proveedor" placement="top">
+                                                        <a href="#" class="cliente-link" @click.prevent="showDialogNewPerson = true">
+                                                            <i class="fas fa-user-plus"></i> [+ Nuevo]
+                                                        </a>
+                                                    </el-tooltip>
+                                                </th>
+                                                <th width="30%" v-if="form.suppliers.length>0" class="pb-2">Correo electrónico</th>
+                                                <th width="15%">
+                                                    <a href="#" @click.prevent="clickAddSupplier" class="cliente-link">
+                                                        <i class="fas fa-plus-circle"></i> [+ Agregar]
+                                                    </a>
+                                                </th>
                                             </tr>
                                         </thead>
-                                        <tbody v-if="form.items.length > 0">
-                                            <tr v-for="(row, index) in form.items" :key="index" width="100%">
-                                                <td width="5%">{{index + 1}}</td>
-                                                <td width="50%">{{row.item.name}}</td>
-                                                <td width="15%" class="text-center">{{row.item.unit_type.name}}</td>
-                                                <td width="15%" class="text-right">{{row.quantity}}</td>
-                                                <td width="15%" class="text-right">
-                                                    <button type="button" class="btn waves-effect waves-light btn-xs btn-danger" @click.prevent="clickRemoveItem(index)">x</button>
+                                        <tbody>
+                                            <tr v-for="(row, index) in form.suppliers" :key="index" width="100%">
+                                                <td width="55%">
+                                                    <div class="form-group mb-1 mr-2">
+                                                        <el-select v-model="row.supplier_id" filterable @change="changeSupplier(index)">
+                                                            <el-option v-for="option in suppliers" :key="option.id" :value="option.id" :label="option.description"></el-option>
+                                                        </el-select>
+                                                    </div>
+                                                </td>
+                                                <td width="30%">
+                                                    <div class="form-group mb-1 mr-2">
+                                                        <el-input v-model="row.email"></el-input>
+                                                    </div>
+                                                </td>
+                                                <td width="15%" class="series-table-actions text-center" v-if="index > 0">
+                                                    <button type="button" class="btn waves-effect waves-light btn-xs btn-danger" @click.prevent="clickCancel(index)">
+                                                        <i class="fa fa-trash"></i>
+                                                    </button>
                                                 </td>
                                             </tr>
-                                            <tr><td colspan="8"></td></tr>
                                         </tbody>
                                     </table>
                                 </div>
-                            </div>
-                            <div class="col-lg-12 col-md-6 d-flex align-items-end">
-                                <div class="form-group">
-                                    <button type="button" class="btn waves-effect waves-light btn-primary" @click.prevent="showDialogAddItem = true">+ Agregar Producto</button>
+
+                                <div class="col-lg-2">
+                                    <div class="form-group" :class="{'has-danger': errors.date_of_issue}">
+                                        <label class="control-label">Fec. Emisión</label>
+                                        <el-date-picker v-model="form.date_of_issue" type="date" value-format="yyyy-MM-dd" :clearable="false" @change="changeDateOfIssue"></el-date-picker>
+                                        <small class="form-control-feedback" v-if="errors.date_of_issue" v-text="errors.date_of_issue[0]"></small>
+                                    </div>
                                 </div>
                             </div>
+                        </div>
 
-
+                        <!-- Productos -->
+                        <div class="form-section">
+                            <h5 class="section-header"><i class="fas fa-list-ul"></i> Productos y Servicios</h5>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="table-responsive">
+                                        <table class="table">
+                                            <thead>
+                                                <tr width="100%">
+                                                    <th width="5%">#</th>
+                                                    <th width="50%" class="font-weight-bold">Descripción</th>
+                                                    <th width="15%" class="text-center font-weight-bold">Unidad</th>
+                                                    <th width="15%" class="text-right font-weight-bold">Cantidad</th>
+                                                    <th width="15%"></th>
+                                                </tr>
+                                            </thead>
+                                            <tbody v-if="form.items.length > 0">
+                                                <tr v-for="(row, index) in form.items" :key="index" width="100%">
+                                                    <td width="5%">{{index + 1}}</td>
+                                                    <td width="50%">{{row.item.name}}</td>
+                                                    <td width="15%" class="text-center">{{row.item.unit_type.name}}</td>
+                                                    <td width="15%" class="text-right">{{row.quantity}}</td>
+                                                    <td width="15%" class="text-right">
+                                                        <button type="button" class="btn waves-effect waves-light btn-xs btn-danger" @click.prevent="clickRemoveItem(index)">x</button>
+                                                    </td>
+                                                </tr>
+                                                <tr><td colspan="8"></td></tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-6 d-flex align-items-end">
+                                    <div class="form-group">
+                                        <button type="button" class="btn waves-effect waves-light btn-primary" @click.prevent="showDialogAddItem = true">
+                                            <i class="fas fa-plus-circle"></i> Agregar Producto
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
                     </div>
 
                     <div class="form-actions text-right mt-4">
                         <el-button @click.prevent="close()">Cancelar</el-button>
-                        <el-button class="submit" type="primary" native-type="submit" :loading="loading_submit" v-if="form.items.length > 0">{{button_text}}</el-button>
+                        <el-button class="submit" type="primary" native-type="submit" :loading="loading_submit" v-if="form.items.length > 0">
+                            <i class="fas fa-save"></i> {{button_text}}
+                        </el-button>
                     </div>
                 </form>
             </div>
@@ -141,6 +156,50 @@
                           :showClose="false"></purchase-quotation-options>
     </div>
 </template>
+
+<style scoped>
+/* Diseño simple y limpio */
+.invoice-form {
+    background: #f8f9fa;
+}
+
+.form-section {
+    background: white;
+    padding: 20px;
+    margin-bottom: 20px;
+    border-radius: 8px;
+    box-shadow: 0 2px 8px rgba(0,0,0,0.08);
+    border-left: 4px solid #409EFF;
+}
+
+.section-header {
+    font-size: 18px;
+    font-weight: 600;
+    color: #2c3e50;
+    margin: 0 0 20px 0;
+    padding-bottom: 10px;
+    border-bottom: 2px solid #e9ecef;
+}
+
+.section-header i {
+    color: #409EFF;
+    margin-right: 10px;
+}
+
+.form-section:nth-child(2) { border-left-color: #67C23A; }
+.form-section:nth-child(3) { border-left-color: #E6A23C; }
+
+.cliente-link {
+    color: #409EFF;
+    font-weight: bold;
+    font-size: 12px;
+    text-decoration: none;
+    transition: color 0.2s;
+}
+
+.cliente-link:hover { color: #66b1ff; }
+.cliente-link i { margin-right: 5px; }
+</style>
 
 <script>
     import QuotationFormItem from './partials/item.vue'
